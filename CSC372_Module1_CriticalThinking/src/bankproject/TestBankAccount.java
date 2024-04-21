cfdc449294e39a9aab12d7641a25c417b48a0ad2
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-04-20 18:27:51 -0700

    Add files via upload

--- CSC372_Module1_CriticalThinking/src/bankproject/TestBankAccount.java
+++ CSC372_Module1_CriticalThinking/src/bankproject/TestBankAccount.java
@@ -1,7 +1,7 @@
 package bankproject;
 
-public class TestBankAccount {
-
+public class TestBankAccount { // Main method to test the CheckingAccount class
+	//Creating a new CheckingAccount object
 	public static void main(String[] args) {
 		CheckingAccount myAccount = new CheckingAccount("Misty", "Ryan", 56789, 2);
 		myAccount.deposit(600);
@@ -9,7 +9,7 @@ public class TestBankAccount {
 		
 		myAccount.processWithdrawal(625);
 		myAccount.displayAccount();
-
+		//The above describes the deposit and withdrawal with an overdraft
 	}
 
 }
