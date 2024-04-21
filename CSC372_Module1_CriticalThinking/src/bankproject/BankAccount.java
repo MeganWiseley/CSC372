83f041118b54f38f3def3cbea6fe4a6ee2b2820b
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-04-20 18:28:39 -0700

    Add files via upload

--- CSC372_Module1_CriticalThinking/src/bankproject/BankAccount.java
+++ CSC372_Module1_CriticalThinking/src/bankproject/BankAccount.java
@@ -1,30 +1,32 @@
 package bankproject;
 
 public class BankAccount {
-	private String firstName;
-	private String lastName;
-	private int accountID;
-	private double balance;
+	private String firstName; //Account holders first name
+	private String lastName; //Account holders last name
+	private int accountID; //Account holders ID
+	private double balance; //Account holders balance
 	
+	// Constructor
 	public BankAccount(String firstName, String lastName, int accountID) {
+		//initialize fields with the provided values
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.accountID = accountID;
-		this.balance = 0.0;
+		this.balance = 0.0; //set initial balance to zero
 	}
-	
+	//Deposit Method (adding amount to balance)
 	public void deposit(double amount) {
 		this.balance += amount;
 	}
-	
+	//Withdrawal Method (subtracting the amount from balance)
 	public void withdrawal(double amount) {
 		this.balance -= amount;
 	}
-	
+	//Getters and Setters below
 	public String getFirstName() {
 		return firstName;
 	}
-	
+	//Setting the first name
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
@@ -32,7 +34,7 @@ public class BankAccount {
 	public String getLastName() {
 		return lastName;
 	}
-	
+	//Setting the last name
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
@@ -40,7 +42,7 @@ public class BankAccount {
 	public int getAccountID() {
 		return accountID;
 	}
-	
+	//Setting the account ID
 	public void setAccountID(int accountID) {
 		this.accountID = accountID;
 	}
@@ -48,7 +50,7 @@ public class BankAccount {
 	public double getBalance() {
 		return balance;
 	}
-	
+	//Printing out the account Summary
 	public void accountSummary() {
 		System.out.println("Account ID: " + accountID);
 		System.out.println("Account Holder: " + firstName + " " + lastName);
--- CSC372_Module1_CriticalThinking/src/bankproject/CheckingAccount.java
+++ CSC372_Module1_CriticalThinking/src/bankproject/CheckingAccount.java
@@ -1,13 +1,13 @@
 package bankproject;
 
-public class CheckingAccount extends BankAccount {
-	private double interestRate;
+public class CheckingAccount extends BankAccount { // CheckingAccount inherits the BankAccount
+	private double interestRate; //Interest rate for the checking account
 	
 	public CheckingAccount(String firstName, String lastName, int accountID, double interestRate) {
-		super(firstName, lastName, accountID);
-		this.interestRate = interestRate;
+		super(firstName, lastName, accountID); // calling the constructor of the superclass 
+		this.interestRate = interestRate; // initialize the interest rate
 	}
-	
+	// Method to process Withdrawals including an overdraft fee
 	public void processWithdrawal(double amount) {
 		if (getBalance() - amount < 0) {
 			super.withdrawal(amount + 30.0);
@@ -16,16 +16,16 @@ public class CheckingAccount extends BankAccount {
 			super.withdrawal(amount);
 		}
 	}
-	
+	// Method to display the Account Summary
 	public void displayAccount() {
 		super.accountSummary();
 		System.out.println("Interest rate: " + interestRate + "%");
 	}
-	
+	// Getter method for retrieving the interest rate
 	public double getInterestRate() {
 		return interestRate;
 	}
-	
+	// Setter method for updating the interest rate
 	public void setInterestRate(double interestRate) {
 		this.interestRate = interestRate;
 	}
