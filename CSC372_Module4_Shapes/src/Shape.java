2f9cdbdfcf78900da5c197b9485f2870f471b9d7
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-05-11 15:45:39 -0700

    Add files via upload

---
+++ CSC372_Module4_Shapes/src/Shape.java
@@ -0,0 +1,9 @@
+
+// Abstract class 
+abstract class Shape {
+	
+	// Abstract Method used to determine the surface area of a shape
+	abstract double surfaceArea();
+	// Abstract Method used to determine the volume of a shape
+	abstract double volume();
+}
