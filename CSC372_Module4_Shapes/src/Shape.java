0028e51d2c5763eca7f28c26419aa956860ec0cf
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-05-11 15:49:09 -0700

    Delete CSC372_Module4_Shapes directory

--- CSC372_Module4_Shapes/src/Shape.java
+++
@@ -1,9 +0,0 @@
-
-// Abstract class 
-abstract class Shape {
-	
-	// Abstract Method used to determine the surface area of a shape
-	abstract double surfaceArea();
-	// Abstract Method used to determine the volume of a shape
-	abstract double volume();
-}
